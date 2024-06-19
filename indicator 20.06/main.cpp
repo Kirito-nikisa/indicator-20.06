e9788921480f828ab4ee81d9a22f49c0d0332e95
Kirito-nikisa <nikosa090@gmail.com>
2024-06-19 18:41:52 +0300

    Added lab ex3

--- indicator 20.06/main.cpp
+++ indicator 20.06/main.cpp
@@ -1,5 +1,56 @@
 #include <iostream>
 using namespace std;
+
 int main() {
 
+    int M;
+    cout << "Enter the size of array A: ";
+    cin >> M;
+
+    int* A = new int[M];
+
+ 
+    cout << "Enter elements of array A: ";
+    for (int i = 0; i < M; i++) {
+        cin >> A[i];
+    }
+
+    
+    int choice;
+    cout << "What do you want to remove? Enter 1 for even numbers, 2 for odd numbers: ";
+    cin >> choice;
+
+  
+    int count = 0;
+    for (int i = 0; i < M; i++) {
+        if (choice == 1 && A[i] % 2 != 0) {
+            count++;
+        }
+        if (choice == 2 && A[i] % 2 == 0) {
+            count++;
+        }
+    }
+
+    int* B = new int[count];
+    int index = 0;
+
+
+    for (int i = 0; i < M; i++) {
+        if (choice == 1 && A[i] % 2 != 0) {
+            B[index++] = A[i];
+        }
+        if (choice == 2 && A[i] % 2 == 0) {
+            B[index++] = A[i];
+        }
+    }
+    cout << "Elements of array B: ";
+    for (int i = 0; i < count; i++) {
+        cout << B[i] << " ";
+    }
+    cout << endl;
+
+    delete[] A;
+    delete[] B;
+
+    return 0;
 }
